74beba1336d84201f33e3e228d95d425c2052f1e
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-09-14 18:38:58 +0100

    install dotenv + replace api keys

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -3,65 +3,66 @@ import AddButton from "../Button";
 import ModalButton from "../ModalButton";
 
 function Menu({ recipes, setRecipes, basketItems, setBasketItems }) {
-	useEffect(() => {
-		async function getRecipesOnLoad() {
-			let response = await fetch(
+    useEffect(() => {
+        async function getRecipesOnLoad() {
+            let response = await fetch(
+                `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=thai&number=10`
+            );
+            // console.log("response: ", response);
+            let data = await response.json();
+            // console.log("data: ", data);
+            let results = data.results;
+            // console.log("results: ", results);
+            setRecipes(results);
+            return results;
+        }
+        getRecipesOnLoad();
+    }, [setRecipes]);
 
-				"https://api.spoonacular.com/recipes/complexSearch?apiKey=1e0627209aa14f53a49187d72790693f&cuisine=thai&number=10"
+    function addtoBasket(itemName) {
+        console.log("item name:", itemName);
+        setBasketItems([...basketItems, itemName]);
+        console.log([...basketItems, itemName]);
+        localStorage.setItem("basket", [...basketItems, itemName]);
+        // localStorage.setItem('basket', localStorage.getItem('basket') + ',' + itemName);
+    }
 
-			);
-			// console.log("response: ", response);
-			let data = await response.json();
-			// console.log("data: ", data);
-			let results = data.results;
-			// console.log("results: ", results);
-			setRecipes(results);
-			return results;
-		}
-		getRecipesOnLoad();
-	}, [setRecipes]);
-
-	function addtoBasket(itemName) {
-		console.log("item name:", itemName);
-		setBasketItems([...basketItems, itemName]);
-		console.log([...basketItems, itemName]);
-		localStorage.setItem("basket", [...basketItems, itemName]);
-		// localStorage.setItem('basket', localStorage.getItem('basket') + ',' + itemName);
-	}
-
-	return (
-		<>
-			<main>
-				{/* <h2>Menu</h2> */}
-				<div className="recipes">
-					{recipes ? (
-						recipes.map((recipe) => {
-							return (
-								<div>
-									<h2>{recipe.title}</h2>
-									<img src={recipe.image} alt={recipe.title} />
-									<AddButton
-										onClick={() => {
-											addtoBasket(recipe.title);
-										}}
-									/>
-									<ModalButton
-										id={recipe.id}
-										name={recipe.title}
-										onClick={() => {
-											addtoBasket(recipe.title);
-										}}
-									/>
-								</div>
-							);
-						})
-					) : (
-						<>Loading...</>
-					)}
-				</div>
-			</main>
-		</>
-	);
+    return (
+        <>
+            <main>
+                {/* <h2>Menu</h2> */}
+                <div className="recipes">
+                    {recipes ? (
+                        recipes.map((recipe) => {
+                            return (
+                                <div>
+                                    <h2>{recipe.title}</h2>
+                                    <img
+                                        src={recipe.image}
+                                        alt={recipe.title}
+                                    />
+                                    <AddButton
+                                        onClick={() => {
+                                            addtoBasket(recipe.title);
+                                        }}
+                                    />
+                                    <ModalButton
+                                        id={recipe.id}
+                                        name={recipe.title}
+                                        onClick={() => {
+                                            addtoBasket(recipe.title);
+                                        }}
+                                    />
+                                </div>
+                            );
+                        })
+                    ) : (
+                        <>Loading...</>
+                    )}
+                </div>
+            </main>
+        </>
+    );
 }
 
 export default Menu;
--- src/Components/ModalButton/index.js
+++ src/Components/ModalButton/index.js
@@ -17,8 +17,7 @@ function ModalButton({ id, name, onClick }) {
 
     async function getRecipeInfo(id) {
         let response = await fetch(
-
-            `https://api.spoonacular.com/recipes/${id}/information?apiKey=1e0627209aa14f53a49187d72790693f`
+            `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.REACT_APP_API_KEY}`
         );
 
         let data = await response.json();
@@ -28,46 +27,53 @@ function ModalButton({ id, name, onClick }) {
         return createMarkup(data.summary);
     }
     function createMarkup() {
-        console.log("creatmarkupcalled!")
+        console.log("creatmarkupcalled!");
         return { __html: `${summary}` };
     }
 
     const { isOpen, onOpen, onClose } = useDisclosure();
     return (
-			<>
-				<Button
-					colorScheme="teal"
-                    className="modal-button"
-					variant="outline"
-                    color="white"
-					onClick={() => {
-						onOpen();
-						getRecipeInfo(id);
-					}}
-				>
-					Open Modal
-				</Button>
+        <>
+            <Button
+                colorScheme="teal"
+                className="modal-button"
+                variant="outline"
+                color="white"
+                onClick={() => {
+                    onOpen();
+                    getRecipeInfo(id);
+                }}
+            >
+                Open Modal
+            </Button>
 
-				<Modal isOpen={isOpen} onClose={onClose}>
-					<ModalOverlay />
-					<ModalContent>
-						<ModalHeader>{name}</ModalHeader>
-						<ModalCloseButton />
-						<ModalBody>
-							<div dangerouslySetInnerHTML={createMarkup()}></div>
-						</ModalBody>
-
-						<ModalFooter>
-							<Button colorScheme="teal" variant="solid" onClick={onClick}>
-								Add to Basket
-							</Button>
-							<Button colorScheme="teal" variant="outline" onClick={onClose}>
-								Close
-							</Button>
-						</ModalFooter>
-					</ModalContent>
-				</Modal>
-			</>
-		);
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>{name}</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <div dangerouslySetInnerHTML={createMarkup()}></div>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button
+                            colorScheme="teal"
+                            variant="solid"
+                            onClick={onClick}
+                        >
+                            Add to Basket
+                        </Button>
+                        <Button
+                            colorScheme="teal"
+                            variant="outline"
+                            onClick={onClose}
+                        >
+                            Close
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    );
 }
 export default ModalButton;
